dce321a6015b7dbda3085aa5a17205074fbc86f0
Rob McKenzie <mckenro@gmail.com>
2017-03-27 10:21:57 -0700

    add contacts and other columns to DB

--- app/app.php
+++ app/app.php
@@ -1,6 +1,7 @@
 <?php
   require_once __DIR__.'/../vendor/autoload.php';
   require_once __DIR__.'/../src/Task.php';
+  require_once __DIR__.'/../src/Category.php';
 
   $app = new Silex\Application();
 
@@ -13,20 +14,39 @@
     'twig.path' => __DIR__.'/../views'
   ));
 
-  $app->get("/", function() use ($app){
-    return $app['twig']->render('task.html.twig', array('tasks' => Task::getAll()));
+  $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
   });
 
-  $app->post("/tasks", function() use ($app){
-    $task = new Task($_POST['description']);
-    $task->save();
-    return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
+  $app->get("/tasks", function() use ($app) {
+      return $app['twig']->render('task.html.twig', array('tasks' => Task::getAll()));
   });
-  
-  $app->post('/delete_tasks', function() use ($app){
-    Task::deleteAll();
-    return $app['twig']->render('delete_task.html.twig');
 
+  $app->get("/categories", function() use ($app) {
+      return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
   });
+
+  $app->post("/categories", function() use ($app) {
+      $category = new Category($_POST['name']);
+      $category->save();
+      return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+  });
+
+  $app->post("/delete_categories", function() use ($app) {
+      Category::deleteAll();
+      return $app['twig']->render('index.html.twig');
+  });
+
+  $app->post("/tasks", function() use ($app) {
+      $task = new Task($_POST['description']);
+      $task->save();
+      return $app['twig']->render('task.html.twig', array('tasks' => Task::getAll()));
+  });
+
+  $app->post("/delete_tasks", function() use ($app) {
+      Task::deleteAll();
+      return $app['twig']->render('index.html.twig');
+  });
+
   return $app;
  ?>
